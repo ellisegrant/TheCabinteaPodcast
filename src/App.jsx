1ae9fe20666ae2427df97fd466135c4137d49215
ellise_grant <101023936+ellisegrant@users.noreply.github.com>
2026-03-19 14:03:47 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import AboutHost from "./Pages/AboutHost";
 import Episodes from "./Pages/Episodes";
 import Partner from "./Pages/Partner";
 import Shop from "./Pages/Shop";
@@ -10,12 +11,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/episodes" element={<Episodes />} />
-        <Route path="/partner" element={<Partner />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/"            element={<Home />} />
+        <Route path="/about"       element={<About />} />
+        <Route path="/about/host"  element={<AboutHost />} />
+        <Route path="/episodes"    element={<Episodes />} />
+        <Route path="/partner"     element={<Partner />} />
+        <Route path="/shop"        element={<Shop />} />
+        <Route path="/contact"     element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
