0c25e630c1068f08ddfccb1fe43acc77985ff290
ellise_grant <101023936+ellisegrant@users.noreply.github.com>
2026-04-06 10:23:10 +0000

    Change hero image source from JPG to PNG

--- src/Pages/AfricaOceanDynamism.jsx
+++ src/Pages/AfricaOceanDynamism.jsx
@@ -95,7 +95,7 @@ export default function AfricaOceanDynamism() {
       }}>
         {/* Hero image */}
         <img
-          src="/maritimetech1.jpg"
+          src="/maritimetech1.png"
           alt="Africa Ocean Dynamism"
           style={{ position: "absolute", inset: 0, width: "100%", height: "100%", objectFit: "cover" }}
         />
